08ef7eb33fe254b22f6756faae81397617bc8fe8
Tichaona Brendon Nyeke <sleepingchickensoftware@gmail.com>
2022-02-18 18:32:10 +0200

    Photo Gallary
    - from https://www.reactnative.express/exercises/photo_gallery tutorial.

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useReducer, useCallback } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+import { getList } from "./api/picsum";
+import { actionCreators, initialState, reducer } from "./reducers/photos";
+import PhotoGrid from "./components/PhotoGrid";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const { photos, nextPage, loading, error } = state;
+
+    const fetchPhotos = useCallback(async () => {
+        dispatch(actionCreators.loading());
+
+        try {
+            const nextPhotos = await getList(nextPage);
+            dispatch(actionCreators.success(nextPhotos, nextPage));
+        } catch (e) {
+            dispatch(actionCreators.failure());
+        }
+    }, [nextPage]);
+
+    useEffect(() => fetchPhotos(nextPage), []);
+
+    if (photos.length === 0 && loading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator animating={true} />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.container}>
+                <Text>Error! Failed to load Images!</Text>
+            </View>
+        );
+    }
+
+    return <PhotoGrid numColumns={3} photos={photos} onEndReached={fetchPhotos} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
